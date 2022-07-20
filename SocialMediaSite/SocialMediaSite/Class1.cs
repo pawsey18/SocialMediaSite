4af31159a1986f37ee400f692eb12dfd0651b365
Nick <pawsey18@gmail.com>
2022-07-20 12:58:56 -0300

    Initial Commit

---
+++ SocialMediaSite/SocialMediaSite/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SocialMediaSite
+{
+    public class Class1
+    {
+    }
+}
---
+++ SocialMediaSite/Types/Types.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+
+namespace Types
+{
+    public struct ParmStruct
+    {
+        public ParmStruct(string name, object value,
+            SqlDbType dataType, int size = 0, ParameterDirection direction = ParameterDirection.Input)
+        {
+            Name = name;
+            Value = value;
+            Size = size;
+            DataType = dataType;
+            Direction = direction;
+        }
+
+        public string Name;
+        public object Value;
+        public int Size;
+        public SqlDbType DataType;
+        public ParameterDirection Direction;
+
+    }
+
+
+    public enum ErrorType
+    {
+        Model,
+        Business
+    }
+}
